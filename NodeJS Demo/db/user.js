30b61b95a4ffe2f0e255efa60136334d2051c195
Bradley Johnson <bsj0006@uah.edu>
2019-04-02 21:49:53 -0500

    Grab user shows an error for any missing values Added a few more queries

--- NodeJS Demo/db/user.js
+++ NodeJS Demo/db/user.js
@@ -11,6 +11,7 @@ module.exports.UserManager = function (database) {
                 callback(user)
             } else {
                 console.error(err);
+                callback(null)
             }
         });
     }
--- NodeJS Demo/routes/management_route.js
+++ NodeJS Demo/routes/management_route.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+let userManager;
 
 /**
  * This router handles any requests from the management page. Requests here
@@ -7,21 +8,37 @@ var path = require('path');
  *
  * @type {Router|router}
  */
-var router = express.Router();
+let router = express.Router();
 
 //GET http://127.0.0.1/manage -> Send ../html/management.html
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '..', 'html', 'managementTools.html'));
 });
 
+//Add user manager as a variable
 router.setUserManager = function (manager) {
-    userManager = manager
+    userManager = manager;
 };
 
+//GET http://127.0.0.1/manage/user?id=<id value>  ->  A json object containing either user or errors (a list of errors)
 router.get('/user', function (req, res) {
-	userId = req.query.id;
-	userManager.getUser(userId, function(user){
-		res.json(user);
-	})})
-	
+    if (!req.query.id || req.query.id === "") {
+        console.log("Id is required to get user");
+        res.json({errors: ["User ID is required to get user"]})
+    } else {
+        let userId = req.query.id;
+        userManager.getUser(userId, function (user) {
+            if (user) {
+                console.log(`Sending user of id ${userId}`);
+                res.json({user: user});
+            } else {
+                let err = `User with id ${userId} not found`;
+                console.log(err);
+                res.json({errors: [err]})
+            }
+        });
+    }
+});
+
+
 module.exports = router;
