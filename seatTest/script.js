67d68077b1856973e83cabbd0713b6789e81bc8a
jaredmichaelwilson <jmw0055@uah.edu>
2019-04-03 18:24:06 -0500

    Latest html and script for seatmaps

--- seatTest/script.js
+++ seatTest/script.js
@@ -28,16 +28,19 @@ function loadDB()   {
 //global array of clicked seats
 var clickedSeats = [];
 
+
 function onClick(element)  {
    // alert("You clicked " +element.title);
 
-    //if seat is available it can be clicked and turned yellow
+    //if seat is available it can be clicked and turned black
     //this adds seat to the array
     if(element.title == "available") {
         element.title = "selected";
         document.getElementById(element.id).style.backgroundColor = "#232323";
         document.getElementById(element.id).style.color = "#ffffff";
         clickedSeats.push(element.id);
+        var currentPrice = parseFloat(element.value);
+        totalPrice = totalPrice + currentPrice;
     }
     //if seat is selected it can be clicked again to unselect
     //removes seat from array
@@ -50,20 +53,47 @@ function onClick(element)  {
         document.getElementById(element.id).style.color = "#fff";
         clickedSeats.splice( clickedSeats.indexOf(element.id), 1 );
         element.title = "available";
+        var currentPrice = parseFloat(element.value);
+        totalPrice = totalPrice - currentPrice;
     }
     txt = "Tickets Selected: ";
+    totalPriceText = "Total Price: $";
+    totalPriceText = totalPriceText + totalPrice;
     clickedSeats.forEach(printTXT)
-    document.getElementById('demo').innerHTML = txt;
+    document.getElementById('selected').innerHTML = txt;
+    document.getElementById('price').innerHTML = totalPriceText;
 }
 
 //txt var will be string of seats
 var txt = "Tickets Selected: ";
+var totalPrice = 0;
+var totalPriceText = "Total Price: ";
 
 //function for buy button
 //this button progresses to the next page
 //needs to save tickets selected by user and update DB
 function buyButton()    {
     alert("You want to buy these tickets: " +clickedSeats)
+}
+
+function setTicketPrice() {
+    var input = document.getElementById("setTicketPrice");
+//get all floor seats
+    document.querySelectorAll('[id^=F]').forEach(function(node) {
+        // Do whatever you want with the node object.
+        node.value = input.elements[0].value;
+    });
+//get all balcony
+    document.querySelectorAll('[id^=B]').forEach(function(node) {
+        // Do whatever you want with the node object.
+        node.value = input.elements[1].value;
+    });
+//get all loge
+    document.querySelectorAll('[id^=L]').forEach(function(node) {
+        // Do whatever you want with the node object.
+        node.value = input.elements[1].value;
+    });
+
 }
 
 function printTXT(value, index, array)    {
